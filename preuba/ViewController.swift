6f0925427c1db791003c314d0f7da48f8ca6369a
osasusan <107266592+osasusan@users.noreply.github.com>
2022-09-27 23:53:53 +0200

    preuba dia 27

--- preuba/ViewController.swift
+++ preuba/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tvName: UILabel!
+    
+    @IBOutlet weak var etName: UITextField!
+    
+    @IBOutlet weak var tvpass: UILabel!
+    
+    @IBOutlet weak var etpass: UITextField!
+    
+    @IBOutlet weak var btLogin: UIButton!
+    
+    @IBOutlet weak var btRegister: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+              
+        //var string = String?(etName)
+        
     }
 
+    @IBAction func presBoton(_ sender: Any) {
+        
+        guard etName.text != nil else {
+            return
+        }
+            
+        
+ 
+    
+    
+    }
 
 }
-
